2b60889eb6d504768a41be71346e81c0fd4a937d
Ben Nagy <ben.nagy.web@gmail.com>
2023-06-19 13:12:52 +0200

    start messing with myst 3 6 similarity

--- src/preproc.py
+++ src/preproc.py
@@ -1,6 +1,8 @@
 import logging
 import os
 import string
+import pandas as pd  # type:ignore
+import re
 
 
 def _preprocess_text(text):
@@ -12,19 +14,17 @@ def _preprocess_text(text):
     return text
 
 
-def _slice_text_into_samples(text, sample_size=1000):
-    text = text.split()
-    num_samples = len(text) // sample_size
-    samples = [
-        text[i * sample_size : (i + 1) * sample_size] for i in range(num_samples)
-    ]
-
+def _overlapping_samples(text: str, sample_size, overlap_ratio: int) -> list[list[str]]:
+    txt_ary: list[str] = text.split()
+    num_samples = len(txt_ary) // (sample_size // overlap_ratio)
+    step = sample_size // overlap_ratio
+    samples = [txt_ary[i * step : (i * step) + sample_size] for i in range(num_samples)]
     return samples
 
 
 def preprocess_and_slice_text_files(
-    folder_path: str, sample_size: int = 1000
-) -> dict[str, list]:
+    folder_path: str, sample_size: int = 1000, overlap_ratio: int = 1
+) -> pd.DataFrame:
     """
     Preprocess a set of text samples. Files are lowercased, split into words,
     and then broken into chunks of the selected size (default 1000 words)
@@ -44,13 +44,15 @@ def preprocess_and_slice_text_files(
     """
     processed_texts = {}
     for filename in os.listdir(folder_path):
-        if filename.endswith(".txt"):  # i'm skipping the Mysery files
+        if filename.endswith(".txt"):  # i'm skipping the Mystery files
             with open(
                 os.path.join(folder_path, filename), "r", encoding="latin-1"
             ) as file:
                 text = file.read()
                 processed_text = _preprocess_text(text)
-                samples = _slice_text_into_samples(processed_text, sample_size)
+                samples = _overlapping_samples(
+                    processed_text, sample_size, overlap_ratio
+                )
                 num_samples = len(samples)
                 for i, sample in enumerate(samples):
                     variable_name = filename[:-4] + "_" + str(i)
@@ -58,5 +60,22 @@ def preprocess_and_slice_text_files(
             logging.info(
                 f"'{filename}' was processed and split into {num_samples} samples"
             )
+    entries = []
 
-    return processed_texts
+    for k, txt in processed_texts.items():
+        # grab the part before the chunk number in the key, split into translator
+        # and work. `if x` drops empty strings that come from re.split.
+        ww = [x for x in re.split("([A-Z][a-z]*)", k.split("_")[0]) if x]
+        transl = ww[0]
+        work = "".join(ww[1:])
+        chunk = " ".join(txt)
+        entries.append(
+            {
+                "Translator": transl,
+                "Chunk": chunk,
+                "Work": work,
+            }
+        )
+    chunk_df = pd.DataFrame(entries)
+
+    return chunk_df
